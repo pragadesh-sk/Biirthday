78ac90013375ad06dc3e0b8c2f5b589955c0fee5
PRAGADESH S K <88279902+pragadesh-sk@users.noreply.github.com>
2024-12-24 11:21:17 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    const surpriseMessage = document.getElementById('surpriseMessage');
+    surpriseMessage.classList.toggle('hidden');
+});
